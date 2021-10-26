16545b6fd6bba23f20f62ea635b3d0c703f2178d
Kamran Ahmed <91557301+coderkamran@users.noreply.github.com>
2021-10-26 15:42:10 +0600

    Update Pizza Lists pizza.dart

--- Pizza Lists pizza.dart
+++ Pizza Lists pizza.dart
@@ -26,8 +26,8 @@ class Pizza extends StatelessWidget {
             leading: CircleAvatar(
               backgroundImage: AssetImage('assets/mrkk.jpg')
             ),
-            title: Text("Mutton Pizza"),
-            subtitle: Text("Its feel tasty"),
+            title: Text("Mutton Pizza", textAlign: TextAlign.center ),
+            subtitle: Text("Its feel tasty", textAlign: TextAlign.center ),
             trailing: Text("20\$"),
           ),
 
@@ -35,16 +35,16 @@ class Pizza extends StatelessWidget {
             leading: CircleAvatar(
               backgroundImage: AssetImage('assets/mrkk.jpg')
             ),
-            title: Text("Bief Pizza"),
-            subtitle: Text("Its feel tasty"),
+            title: Text("Bief Pizza", textAlign: TextAlign.center ),
+            subtitle: Text("Its feel tasty", textAlign: TextAlign.center ),
             trailing: Text("35\$"),
           ),
            ListTile(
             leading: CircleAvatar(
               backgroundImage: AssetImage('assets/mrkk.jpg')
             ),
-            title: Text("Milk Pizza"),
-            subtitle: Text("Its feel tasty"),
+            title: Text("Milk Pizza", textAlign: TextAlign.center ),
+            subtitle: Text("Its feel tasty", textAlign: TextAlign.center ),
             trailing: Text("25\$"),
           ),
 
@@ -52,8 +52,8 @@ class Pizza extends StatelessWidget {
             leading: CircleAvatar(
               backgroundImage: AssetImage('assets/mrkk.jpg')
             ),
-            title: Text("Rice Pizza"),
-            subtitle: Text("Its feel tasty"),
+            title: Text("Rice Pizza", textAlign: TextAlign.center ),
+            subtitle: Text("Its feel tasty", textAlign: TextAlign.center ),
             trailing: Text("15\$"),
           ),
 
@@ -61,8 +61,8 @@ class Pizza extends StatelessWidget {
             leading: CircleAvatar(
               backgroundImage: AssetImage('assets/mrkk.jpg')
             ),
-            title: Text("Vegetable Pizza"),
-            subtitle: Text("Its feel tasty"),
+            title: Text("Vegetable Pizza", textAlign: TextAlign.center ),
+            subtitle: Text("Its feel tasty", textAlign: TextAlign.center ),
             trailing: Text("10\$"),
           ),
 
@@ -70,8 +70,8 @@ class Pizza extends StatelessWidget {
             leading: CircleAvatar(
               backgroundImage: AssetImage('assets/evomaxit-office.jpg')
             ),
-            title: Text("Chicken Pizza"),
-            subtitle: Text("Its feel tasty"),
+            title: Text("Chicken Pizza", textAlign: TextAlign.center ),
+            subtitle: Text("Its feel tasty", textAlign: TextAlign.center ),
             trailing: Text("15\$"),
           ),
 
@@ -79,8 +79,8 @@ class Pizza extends StatelessWidget {
             leading: CircleAvatar(
                backgroundImage: AssetImage('assets/ragdoll-cat.jpg')
             ),
-            title: Text("Mutton Pizza"),
-            subtitle: Text("Its feel tasty"),
+            title: Text("Mutton Pizza", textAlign: TextAlign.center ),
+            subtitle: Text("Its feel tasty", textAlign: TextAlign.center ),
             trailing: Text("15\$"),
           ),
 
@@ -88,8 +88,8 @@ class Pizza extends StatelessWidget {
             leading: CircleAvatar(
                backgroundImage: AssetImage('assets/persiancat.jpg')
             ),
-            title: Text("Bief Pizza"),
-            subtitle: Text("Its feel tasty"),
+            title: Text("Bief Pizza", textAlign: TextAlign.center ),
+            subtitle: Text("Its feel tasty", textAlign: TextAlign.center ),
             trailing: Text("15\$"),
           ),
 
@@ -97,8 +97,8 @@ class Pizza extends StatelessWidget {
             leading: CircleAvatar(
                backgroundImage: AssetImage('assets/pizza-tomato.jpg')
             ),
-            title: Text("Palok Pizza"),
-            subtitle: Text("Its feel tasty"),
+            title: Text("Palok Pizza", textAlign: TextAlign.center ),
+            subtitle: Text("Its feel tasty", textAlign: TextAlign.center ),
             trailing: Text("15\$"),
           ),
 
@@ -106,8 +106,8 @@ class Pizza extends StatelessWidget {
             leading: CircleAvatar(
                backgroundImage: AssetImage('assets/evomaxit-office.jpg')
             ),
-            title: Text("Eggs Pizza"),
-            subtitle: Text("Its feel tasty"),
+            title: Text("Eggs Pizza", textAlign: TextAlign.center ),
+            subtitle: Text("Its feel tasty", textAlign: TextAlign.center ),
             trailing: Text("15\$"),
           ),
         ],
